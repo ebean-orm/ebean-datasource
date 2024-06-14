e970346694f294b7314df623c89fb548641a70d3
Rob Bygrave <robin.bygrave@gmail.com>
2024-06-15 00:18:38 +1200

    Remove Deprecated methods from DataSourcePool and PoolStatus (migrate to renamed methods)

--- ebean-datasource-api/src/main/java/io/ebean/datasource/PoolStatus.java
+++ ebean-datasource-api/src/main/java/io/ebean/datasource/PoolStatus.java
@@ -11,25 +11,9 @@ public interface PoolStatus {
   int minSize();
 
   /**
-   * Deprecated migrate to minSize()
-   */
-  @Deprecated
-  default int getMinSize() {
-    return minSize();
-  }
-
-  /**
    * Return the pools maximum size.
    */
   int maxSize();
-
-  /**
-   * Deprecated migrate to maxSize()
-   */
-  @Deprecated
-  default int getMaxSize() {
-    return maxSize();
-  }
 
   /**
    * Return number of free connections.
@@ -37,25 +21,9 @@ public interface PoolStatus {
   int free();
 
   /**
-   * Deprecated migrate to free()
-   */
-  @Deprecated
-  default int getFree() {
-    return free();
-  }
-
-  /**
    * Return number of busy connections.
    */
   int busy();
-
-  /**
-   * Deprecated migrate to busy()
-   */
-  @Deprecated
-  default int getBusy() {
-    return busy();
-  }
 
   /**
    * Return the number of threads waiting for connections.
@@ -63,25 +31,9 @@ public interface PoolStatus {
   int waiting();
 
   /**
-   * Deprecated migrate to waiting()
-   */
-  @Deprecated
-  default int getWaiting() {
-    return waiting();
-  }
-
-  /**
    * Return the busy connection high water mark.
    */
   int highWaterMark();
-
-  /**
-   * Deprecated migrate to waiting()
-   */
-  @Deprecated
-  default int getHighWaterMark() {
-    return highWaterMark();
-  }
 
   /**
    * Return the number of times threads had to wait for connections.
@@ -89,25 +41,9 @@ public interface PoolStatus {
   int waitCount();
 
   /**
-   * Deprecated migrate to waitCount()
-   */
-  @Deprecated
-  default int getWaitCount() {
-    return waitCount();
-  }
-
-  /**
    * Return the hit count against the pool.
    */
   int hitCount();
-
-  /**
-   * Deprecated migrate to hitCount()
-   */
-  @Deprecated
-  default int getHitCount() {
-    return hitCount();
-  }
 
   /**
    * Return the max acquire time in micros.
