b026481fa04e4465f6323d997d6b238d2e5f1742
Roland Praml <roland.praml@foconis.de>
2025-02-27 14:59:43 +0100

    Simplified test

--- ebean-datasource/src/test/java/io/ebean/datasource/pool/ConnectionPoolRecoverTest.java
+++ ebean-datasource/src/test/java/io/ebean/datasource/pool/ConnectionPoolRecoverTest.java
@@ -15,9 +15,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class ConnectionPoolRecoverTest {
 
-  private int success;
-  private int failure;
-
   @Test
   void testHeavyLoadPool() throws Exception {
     DataSourcePool pool = DataSourceBuilder.create()
@@ -31,34 +28,15 @@ public class ConnectionPoolRecoverTest {
       //     .heartbeatMaxPoolExhaustedCount(1)
       .failOnStart(false)
       .build();
-    ExecutorService executorService = Executors.newCachedThreadPool();
     try {
-      List<Future<?>> futures = new ArrayList<>();
-      for (int i = 0; i < 2; i++) {
-        futures.add(executorService.submit(() -> doSomeWork(pool)));
+      for (int i = 0; i < 5; i++) {
+        try (Connection conn = pool.getConnection()) {
+          Thread.sleep(2000);
+          conn.rollback();
+        }
       }
-
-      for (Future<?> future : futures) {
-        future.get();
-      }
-      assertThat(success).isGreaterThan(1);
     } finally {
-      executorService.shutdownNow();
       pool.shutdown();
-    }
-
-  }
-
-  private void doSomeWork(DataSourcePool pool) {
-    for (int i = 0; i < 20; i++) {
-      System.out.println("Success: " + success + " failure: " + failure);
-      try (Connection conn = pool.getConnection()) {
-        Thread.sleep(2000);
-        success++;
-      } catch (Exception e) {
-        // nop
-        failure++;
-      }
     }
   }
 
