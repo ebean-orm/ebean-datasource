b41fe18273ad7679f262b3fcd13850830e245a2a
Roland Praml <roland.praml@foconis.de>
2025-03-28 13:40:28 +0100

    refactored the list in separate classes

--- ebean-datasource/src/test/java/io/ebean/datasource/pool/ConnectionBufferTest.java
+++ ebean-datasource/src/test/java/io/ebean/datasource/pool/ConnectionBufferTest.java
@@ -19,23 +19,20 @@ class ConnectionBufferTest {
 
 
     assertEquals(0, b.freeSize());
-    assertFalse(b.hasFreeConnections());
 
     b.addFree(p0);
 
     assertEquals(1, b.freeSize());
-    assertTrue(b.hasFreeConnections());
 
-    PooledConnection r0 = b.popFree(null);
+    PooledConnection r0 = b.removeFree(null);
     b.addBusy(p0);
     assertThat(p0).isSameAs(r0);
 
     assertEquals(0, b.freeSize());
-    assertFalse(b.hasFreeConnections());
 
     assertEquals(0, b.freeSize());
     assertEquals(1, b.busySize());
-    b.moveToFreeList(p0);
+    b.addFree(p0);
     assertEquals(1, b.freeSize());
     assertEquals(0, b.busySize());
 
@@ -44,43 +41,43 @@ class ConnectionBufferTest {
 
     assertEquals(3, b.freeSize());
 
-    PooledConnection r1 = b.popFree(null);
+    PooledConnection r1 = b.removeFree(null);
     b.addBusy(r1);
     assertSame(p2, r1);
-    PooledConnection r2 = b.popFree(null);
+    PooledConnection r2 = b.removeFree(null);
     b.addBusy(r2);
     assertSame(p1, r2);
 
     assertEquals(1, b.freeSize());
-    b.moveToFreeList(r1);
+    b.addFree(r1);
 
     assertEquals(2, b.freeSize());
-    PooledConnection r3 = b.popFree(null);
+    PooledConnection r3 = b.removeFree(null);
     b.addBusy(r3);
     assertSame(p2, r3);
     assertEquals(1, b.freeSize());
-    PooledConnection r4 = b.popFree(null);
+    PooledConnection r4 = b.removeFree(null);
     b.addBusy(r4);
     assertSame(p0, r4);
     assertEquals(0, b.freeSize());
 
-    b.moveToFreeList(r3); // = p2
-    b.moveToFreeList(r2); // = p1
-    b.moveToFreeList(r4); // = p0
+    b.addFree(r3);// = p2
+    b.addFree(r2);// = p1
+    b.addFree(r4);// = p0
 
     assertEquals(3, b.freeSize());
 
-    PooledConnection r5 = b.popFree(null);
+    PooledConnection r5 = b.removeFree(null);
     b.addBusy(r5);
     assertSame(p0, r5);
     assertEquals(2, b.freeSize());
 
-    PooledConnection r6 = b.popFree(null);
+    PooledConnection r6 = b.removeFree(null);
     b.addBusy(r6);
     assertSame(p1, r6);
     assertEquals(1, b.freeSize());
 
-    PooledConnection r7 = b.popFree(null);
+    PooledConnection r7 = b.removeFree(null);
     b.addBusy(r7);
     assertSame(p2, r7);
     assertEquals(0, b.freeSize());
@@ -112,43 +109,48 @@ class ConnectionBufferTest {
     assertEquals(2, b.busySize());
     assertEquals(2, b.freeSize());
 
-    PooledConnection c3 = b.popFree(null);
+    PooledConnection c3 = b.removeFree(null);
     assertSame(p3, c3);
     assertEquals(2, b.busySize());
     assertEquals(1, b.freeSize());
 
     b.addBusy(c3);
+    assertEquals(3, b.busySize());
+    assertEquals(1, b.freeSize());
     assertThatThrownBy(() -> b.addBusy(p3)).hasMessageContaining("Node already member of a list");
     assertEquals(3, b.busySize());
 
-    PooledConnection c2 = b.popFree(null);
+    PooledConnection c2 = b.removeFree(null);
     b.addBusy(c2);
     assertSame(p2, c2);
 
     assertEquals(4, b.busySize());
     assertEquals(0, b.freeSize());
 
-    assertNull(b.popFree(null)); // no free connections left
+    assertNull(b.removeFree(null)); // no free connections left
 
     // all are busy now
-    assertNotNull(p0.busyNode());
-    assertNotNull(p1.busyNode());
-    assertNotNull(p2.busyNode());
-    assertNotNull(p3.busyNode());
+    assertTrue(p0.busyFree().isLinked());
+    assertTrue(p1.busyFree().isLinked());
+    assertTrue(p2.busyFree().isLinked());
+    assertTrue(p3.busyFree().isLinked());
 
     b.removeBusy(p0);
     assertEquals(3, b.busySize());
     assertEquals(0, b.freeSize());
+    assertFalse(p0.busyFree().isLinked());
 
-    assertFalse(b.moveToFreeList(p0));
-    assertTrue(b.moveToFreeList(p1));
-    assertFalse(b.moveToFreeList(p1));
+    assertFalse(b.removeBusy(p0));
+    assertTrue(b.removeBusy(p1));
+    b.addFree(p1);
+    assertFalse(b.removeBusy(p1));
 
     assertEquals(2, b.busySize());
     assertEquals(1, b.freeSize());
 
-    b.moveToFreeList(p2);
-    b.moveToFreeList(p3);
+    b.addFree(p2);
+    b.addFree(p3);
+    b.addFree(p3);
 
     assertEquals(0, b.busySize());
     assertEquals(3, b.freeSize());
@@ -171,8 +173,8 @@ class ConnectionBufferTest {
 
     PooledConnection c1 = getConnection(b, 42);
     PooledConnection c2 = getConnection(b, 17);
-    b.moveToFreeList(c1);
-    b.moveToFreeList(c2);
+    b.addFree(c1);
+    b.addFree(c2);
 
     PooledConnection c3 = getConnection(b,43);
     assertNotSame(c3, c1);
@@ -183,9 +185,9 @@ class ConnectionBufferTest {
   }
 
   private static PooledConnection getConnection(ConnectionBuffer b, Object affinity) {
-    PooledConnection c1 = b.popFree(affinity);
+    PooledConnection c1 = b.removeFree(affinity);
     if (c1 == null) {
-      c1 = b.popFree(ConnectionBuffer.POP_LAST);
+      c1 = b.removeFree(ConnectionBuffer.GET_OLDEST);
     }
     c1.setAffinityId(affinity);
     b.addBusy(c1);
