29b97d3d85e0cd584b8f52dba774ff8ec3c3842c
Rob Bygrave <robin.bygrave@gmail.com>
2025-04-24 12:54:43 +1200

    Remove unused import only

--- ebean-datasource/src/main/java/io/ebean/datasource/pool/ConnectionPool.java
+++ ebean-datasource/src/main/java/io/ebean/datasource/pool/ConnectionPool.java
@@ -8,7 +8,6 @@ import java.sql.*;
 import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
