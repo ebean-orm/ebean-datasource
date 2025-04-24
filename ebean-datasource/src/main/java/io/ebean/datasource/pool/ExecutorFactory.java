b98aaee95114df6f5e30a77558af73916208e484
Rob Bygrave <robin.bygrave@gmail.com>
2025-04-24 18:25:54 +1200

    Internal heartbeat Timer, use Virtual Thread for Java 21
    Using Multi-Version jar, use Virtual Thread instead of Timer for executing the heartbeat test

--- ebean-datasource/src/main/java/io/ebean/datasource/pool/ExecutorFactory.java
+++ ebean-datasource/src/main/java/io/ebean/datasource/pool/ExecutorFactory.java
@@ -1,5 +1,9 @@
 package io.ebean.datasource.pool;
 
+import io.ebean.datasource.pool.ConnectionPool.Heartbeat;
+
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -7,5 +11,42 @@ final class ExecutorFactory {
 
   static ExecutorService newExecutor() {
     return Executors.newSingleThreadExecutor();
+  }
+
+  /**
+   * Return a new Heartbeat for the pool.
+   */
+  static Heartbeat newHeartBeat(ConnectionPool pool, int freqMillis) {
+    final Timer timer = new Timer(pool.name() + ".heartbeat", true);
+    timer.scheduleAtFixedRate(new HeartbeatTask(pool), freqMillis, freqMillis);
+    return new TimerHeartbeat(timer);
+  }
+
+  private static final class TimerHeartbeat implements Heartbeat {
+
+    private final Timer timer;
+
+    private TimerHeartbeat(Timer timer) {
+      this.timer = timer;
+    }
+
+    @Override
+    public void stop() {
+      timer.cancel();
+    }
+  }
+
+  private static final class HeartbeatTask extends TimerTask {
+
+    private final ConnectionPool pool;
+
+    private HeartbeatTask(ConnectionPool pool) {
+      this.pool = pool;
+    }
+
+    @Override
+    public void run() {
+      pool.heartbeat();
+    }
   }
 }
--- ebean-datasource/src/main/java21/io/ebean/datasource/pool/ExecutorFactory.java
+++ ebean-datasource/src/main/java21/io/ebean/datasource/pool/ExecutorFactory.java
@@ -1,11 +1,61 @@
 package io.ebean.datasource.pool;
 
+import io.ebean.datasource.pool.ConnectionPool.Heartbeat;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 final class ExecutorFactory {
 
   static ExecutorService newExecutor() {
     return Executors.newVirtualThreadPerTaskExecutor();
+  }
+
+  static Heartbeat newHeartBeat(ConnectionPool pool, int freqMillis) {
+    return new VTHeartbeat(pool, freqMillis).start();
+  }
+
+  private static final class VTHeartbeat implements Heartbeat {
+
+    private final AtomicBoolean running = new AtomicBoolean(false);
+    private final ConnectionPool pool;
+    private final int freqMillis;
+    private final Thread thread;
+
+    private VTHeartbeat(ConnectionPool pool, int freqMillis) {
+      this.pool = pool;
+      this.freqMillis = freqMillis;
+      this.thread = Thread.ofVirtual()
+              .name(pool.name() + ".heartbeat")
+              .unstarted(this::run);
+    }
+
+    private void run() {
+      while (running.get()) {
+        try {
+          Thread.sleep(freqMillis);
+          pool.heartbeat();
+        } catch (InterruptedException e) {
+          Thread.currentThread().interrupt();
+          break;
+        } catch (Exception e) {
+          // continue heartbeat
+          Log.warn("Error during heartbeat", e);
+        }
+      }
+    }
+
+    private Heartbeat start() {
+      running.set(true);
+      thread.start();
+      return this;
+    }
+
+    @Override
+    public void stop() {
+      running.set(false);
+      thread.interrupt();
+    }
   }
 }
